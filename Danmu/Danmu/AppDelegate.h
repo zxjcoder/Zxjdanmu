8f6a25214d435a78d84a89359b6589bd0d570319
zengxiangjie <zengxiangjie@xiaoyezi.com>
2016-07-31 10:46:14 +0800

    新建项目

---
+++ Danmu/Danmu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Danmu
+//
+//  Created by zxj－mac on 16/7/31.
+//  Copyright © 2016年 zxj－mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Danmu/Danmu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Danmu
+//
+//  Created by zxj－mac on 16/7/31.
+//  Copyright © 2016年 zxj－mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
